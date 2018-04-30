04d0e3ed871af1e132f3b95df0af878b5a0a2bb5
CetaneChris <christopher.raymond@mavs.uta.edu>
2018-04-30 11:04:01 -0500

    Presentation

--- pages/newDevice.php
+++ pages/newDevice.php
@@ -3,13 +3,13 @@
  -->
 <?php include_once ($_SERVER ['DOCUMENT_ROOT'] . '/pages/header.php'); ?>
 <script src="jquery.js"> </script>
-<title>Food Service Vendor New Customer</title>
+<title>New Device</title>
 
 <body>
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">New Customer</h1>
+                <h1 class="page-header">New Device</h1>
             </div>
         </div>
         <!-- /.row -->
@@ -22,75 +22,38 @@
                     <div class="panel-heading">
                         <i class="fa fa-shopping-cart fa-fw"></i> All fields are required
                     </div>
-                    <form name="newcustomer" method= "POST"  action="/pages/insertCustomer.php" onsubmit="return validateForm();">
+                    <!-- <form name="newcustomer" method= "POST"  action="/pages/insertCustomer.php" onsubmit="return validateForm();"> -->
                         <table class="table table-striped">
                             <tr>
-                                <td>Name</td>
+                                <td>Device ID</td>
                                 <td>
                                 	<div class="form-group">
-										<input type="text" class="form-control" id="name" name="name">
+										<input type="number" class="form-control" id="D_ID" name="D_ID"  min="1" step="1">
 									</div>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Phone Number</td>
+                                <td>Room Number</td>
                                 <td>
                                 	<div class="form-group">
-										<input type="telephone" class="form-control" id="pno" name="pno" placeholder="XXX-XXX-XXXX">
+										<input type="number" class="form-control" id="room_num" name="room_num"  min="1" step="1">
 									</div>
                                 </td>
                             </tr>
                             <tr>
-                                <td>Address</td>
+                                <td>Location ID</td>
                                 <td>
                                 	<div class="form-group">
-										<input type="text" class="form-control" id="address" name="address" placeholder="1234 Main Street, Arlington, Texas 76013">
+										<input type="number" class="form-control" id="theater_num" name="theater_num"  min="1" step="1">
 									</div>
                                 </td>
-                            </tr>
-                            <tr>
-                                <td>Email</td>
-                                <td>
-                                	<div class="form-group">
-										<input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="example@server.com">
-										<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-									</div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Username</td>
-                                <td>
-                                	<div class="form-group">
-                                        <input type="text" class="form-control" id="username" name="username">
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Password</td>
-                                <td>
-                                    <div class="form-group">
-										<input type="password" class="form-control" id="password" name="password">
-									</div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Confirm Password</td>
-                                <td>
-                                    <div class="form-group">
-										<input type="password" class="form-control" id="conf_password" name="conf_password">
-									</div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Current Date</td>
-                                <td><?php echo $date = date("m/d/Y", time());?></td>
                             </tr>
                             <tr>
                                 <td><input class="btn btn-primary pull-right" type="reset" value="Reset"></td>
                                 <td><input class="btn btn-primary" type="submit" value="Submit"></td>
                             </tr>
-                        </table>
-                    </form>
+						</table>
+                 <!--    </form> -->
                 </div>
             </div>
         </div>
@@ -101,23 +64,11 @@
 </body>
 <script type="text/javascript">
     function validateForm(){
-    	var name  = document.getElementById("name").value;
-    	var pno   = document.getElementById("pno").value;
-    	var addr  = document.getElementById("address").value;
-    	var email = document.getElementById("email").value;
-    	var uname = document.getElementById("username").value;
-    	var pass  = document.getElementById("password").value;
-    	var conf_pass  = document.getElementById("conf_password").value;
-
-    	if(pass != conf_pass){
-    		document.getElementById('errordiv').style.display = 'block';
-    
-    		document.getElementById("errormessage").innerHTML = "Password fields must match";
-    		
-    		return false;
-    	}
-    
-    	if(name == "" || pno == "" ||  addr == "" || email == "" || uname == "" || pass == ""){
+    	var D_ID        = document.getElementById("D_ID").value;
+    	var room_num    = document.getElementById("room_num").value;
+    	var theater_num = document.getElementById("theater_num").value;
+    	    
+    	if(D_ID == "" || room_num == "" ||  theater_num == ""){
     		document.getElementById('errordiv').style.display = 'block';
     
     		document.getElementById("errormessage").innerHTML = "All fields are required";
