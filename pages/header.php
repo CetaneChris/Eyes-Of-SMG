f3f7c872136eb3ffd5eae710d31ad998638d99fb
CetaneChris <christopher.raymond@mavs.uta.edu>
2018-05-29 09:44:19 -0500

    home page update

--- pages/header.php
+++ pages/header.php
@@ -17,11 +17,6 @@
 	<link href="/vendor/blackrock-digital/css/sb-admin-2.css?=v8" rel="stylesheet">
 	<link href="/vendor/morrisjs/morris.css" rel="stylesheet">
 	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 <?php
 date_default_timezone_set('America/Chicago');
 include_once ($_SERVER['DOCUMENT_ROOT'].'/connections/db_connect8.php');
@@ -32,13 +27,7 @@ include_once ($_SERVER['DOCUMENT_ROOT'].'/class/all_classes.php');?>
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0" id='navbar'>
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" id="navbar-brand" href="/index.php"><img src="/images/SMG.png" width="" height="50" type="image/png"></a>
+                <a class="navbar-brand" id="navbar-brand" href="/index.php"><img src="/images/groupLogo_white.png" width="" height="50" type="image/png"></a>
             </div>
             <!-- /.navbar-header -->
 
@@ -47,12 +36,8 @@ include_once ($_SERVER['DOCUMENT_ROOT'].'/class/all_classes.php');?>
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-                        <li>
-                            <a href="/index.php"><i class="fa fa-building fa-fw"></i> Home</a>
-                        </li><li>
+                    	<li>
                             <a href="/pages/newDevice.php"><i class="fas fa-video fa-fw"></i> New Device</a>
-                        </li><li>
-                            <a href="/pages/reporting.php"><i class="fas fa-chart-line fa-fw"></i> Reporting</a>
                         </li><li>
                             <a href="/pages/js_reporting.php"><i class="fas fa-chart-line fa-fw"></i> NodeJS</a>
                         </li>
