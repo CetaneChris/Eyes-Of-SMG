1cef560953c925e133c23c34d4753c2f0a787c26
CetaneChris <christopher.raymond@mavs.uta.edu>
2018-06-08 17:00:39 -0500

    db_update

--- seniordesign.sql
+++ seniordesign.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 04, 2018 at 02:07 AM
+-- Generation Time: Jun 08, 2018 at 09:57 PM
 -- Server version: 5.7.22-0ubuntu0.16.04.1
 -- PHP Version: 7.0.30-0ubuntu0.16.04.1
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `seniordesign`
 --
+CREATE DATABASE IF NOT EXISTS `seniordesign` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `seniordesign`;
 
 -- --------------------------------------------------------
 
@@ -28,9 +30,9 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `devices`;
 CREATE TABLE IF NOT EXISTS `devices` (
-  `D_ID` INT NOT NULL,
-  `THEATER_NUM` INT NOT NULL,
-  `LOCATION_ID` INT NOT NULL,
+  `D_ID` int(11) NOT NULL,
+  `THEATER_NUM` int(11) NOT NULL,
+  `LOCATION_ID` int(11) NOT NULL,
   UNIQUE KEY `D_ID` (`D_ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -49,7 +51,7 @@ INSERT INTO `devices` VALUES(2, 1, 1);
 
 DROP TABLE IF EXISTS `status`;
 CREATE TABLE IF NOT EXISTS `status` (
-  `ID` INT NOT NULL,
+  `ID` int(11) NOT NULL,
   `MESSAGE` varchar(32) NOT NULL,
   UNIQUE KEY `ID` (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -70,8 +72,10 @@ INSERT INTO `status` VALUES(3, 'Cleaning');
 
 DROP TABLE IF EXISTS `theaters`;
 CREATE TABLE IF NOT EXISTS `theaters` (
-  `ID` INT NOT NULL,
-  `STATUS` INT NOT NULL,
+  `ID` int(11) NOT NULL,
+  `STATUS` int(11) NOT NULL,
+  `MOVIE` varchar(10000) NOT NULL,
+  `REMAINING_TIME` time NOT NULL,
   UNIQUE KEY `ID` (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -79,20 +83,20 @@ CREATE TABLE IF NOT EXISTS `theaters` (
 -- Dumping data for table `theaters`
 --
 
-INSERT INTO `theaters` VALUES(1, 1);
-INSERT INTO `theaters` VALUES(2, 3);
-INSERT INTO `theaters` VALUES(3, 2);
-INSERT INTO `theaters` VALUES(4, 1);
-INSERT INTO `theaters` VALUES(5, 3);
-INSERT INTO `theaters` VALUES(6, 1);
-INSERT INTO `theaters` VALUES(7, 3);
-INSERT INTO `theaters` VALUES(8, 1);
-INSERT INTO `theaters` VALUES(9, 3);
-INSERT INTO `theaters` VALUES(10, 3);
-INSERT INTO `theaters` VALUES(11, 2);
-INSERT INTO `theaters` VALUES(12, 1);
-INSERT INTO `theaters` VALUES(13, 2);
-INSERT INTO `theaters` VALUES(14, 3);
+INSERT INTO `theaters` VALUES(6, 1, 'The Book of Life', '00:07:50');
+INSERT INTO `theaters` VALUES(5, 3, 'Deadpool 2', '00:13:38');
+INSERT INTO `theaters` VALUES(4, 2, 'Solo: A Star Wars Story', '02:19:00');
+INSERT INTO `theaters` VALUES(3, 2, 'Hotel Artemis', '02:22:12');
+INSERT INTO `theaters` VALUES(2, 3, 'Hereditary', '00:23:29');
+INSERT INTO `theaters` VALUES(1, 1, 'Ocean\'s 8', '00:22:15');
+INSERT INTO `theaters` VALUES(7, 3, 'Adrift', '00:07:28');
+INSERT INTO `theaters` VALUES(8, 1, 'Upgrade', '00:15:29');
+INSERT INTO `theaters` VALUES(9, 2, 'Action Point', '01:41:18');
+INSERT INTO `theaters` VALUES(10, 3, 'Avengers: Infinity War', '00:17:32');
+INSERT INTO `theaters` VALUES(11, 2, 'Show Dogs', '01:10:41');
+INSERT INTO `theaters` VALUES(12, 1, 'Breaking In', '00:11:16');
+INSERT INTO `theaters` VALUES(13, 2, 'Book Club', '02:54:07');
+INSERT INTO `theaters` VALUES(14, 3, 'The Life of the Party', '00:11:25');
 
 -- --------------------------------------------------------
 
@@ -102,11 +106,12 @@ INSERT INTO `theaters` VALUES(14, 3);
 
 DROP TABLE IF EXISTS `track_eyes`;
 CREATE TABLE IF NOT EXISTS `track_eyes` (
-  `TRACK_ID` INT NOT NULL AUTO_INCREMENT,
-  `D_ID` INT NOT NULL,
-  `HAS_ATTENTION` INT NOT NULL,
-  `TOTAL` INT NOT NULL,
-  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `TRACK_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `THEATER_NUM` int(11) NOT NULL,
+  `HAS_ATTENTION` int(11) NOT NULL,
+  `TOTAL` int(11) NOT NULL,
+  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `OCCUPIED` varchar(10000) NOT NULL,
   PRIMARY KEY (`TRACK_ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
@@ -114,21 +119,21 @@ CREATE TABLE IF NOT EXISTS `track_eyes` (
 -- Dumping data for table `track_eyes`
 --
 
-INSERT INTO `track_eyes` VALUES(1, 1, 1, 1, '2018-05-04 02:00:00');
-INSERT INTO `track_eyes` VALUES(3, 1, 3, 3, '2018-05-04 02:10:00');
-INSERT INTO `track_eyes` VALUES(2, 1, 2, 3, '2018-05-04 02:05:00');
-INSERT INTO `track_eyes` VALUES(4, 1, 5, 5, '2018-05-04 02:15:00');
-INSERT INTO `track_eyes` VALUES(5, 1, 4, 5, '2018-05-04 02:20:00');
-INSERT INTO `track_eyes` VALUES(6, 1, 6, 6, '2018-05-04 02:25:00');
-INSERT INTO `track_eyes` VALUES(7, 1, 5, 6, '2018-05-04 02:30:00');
-INSERT INTO `track_eyes` VALUES(8, 1, 5, 6, '2018-05-04 02:35:00');
-INSERT INTO `track_eyes` VALUES(9, 1, 6, 8, '2018-05-04 02:40:00');
-INSERT INTO `track_eyes` VALUES(10, 1, 7, 8, '2018-05-04 02:45:00');
-INSERT INTO `track_eyes` VALUES(11, 1, 5, 8, '2018-05-04 02:50:00');
-INSERT INTO `track_eyes` VALUES(12, 1, 5, 8, '2018-05-04 02:55:00');
-INSERT INTO `track_eyes` VALUES(13, 1, 7, 8, '2018-05-04 03:00:00');
-INSERT INTO `track_eyes` VALUES(14, 2, 1, 1, '2018-05-04 02:35:00');
-INSERT INTO `track_eyes` VALUES(15, 2, 1, 1, '2018-05-04 02:40:00');
+INSERT INTO `track_eyes` VALUES(1, 1, 1, 1, '2018-05-04 02:00:00', '');
+INSERT INTO `track_eyes` VALUES(3, 1, 3, 3, '2018-05-04 02:10:00', '');
+INSERT INTO `track_eyes` VALUES(2, 1, 2, 3, '2018-05-04 02:05:00', '');
+INSERT INTO `track_eyes` VALUES(4, 1, 5, 5, '2018-05-04 02:15:00', '');
+INSERT INTO `track_eyes` VALUES(5, 1, 4, 5, '2018-05-04 02:20:00', '');
+INSERT INTO `track_eyes` VALUES(6, 1, 6, 6, '2018-05-04 02:25:00', '');
+INSERT INTO `track_eyes` VALUES(7, 1, 5, 6, '2018-05-04 02:30:00', '');
+INSERT INTO `track_eyes` VALUES(8, 1, 5, 6, '2018-05-04 02:35:00', '');
+INSERT INTO `track_eyes` VALUES(9, 1, 6, 8, '2018-05-04 02:40:00', '');
+INSERT INTO `track_eyes` VALUES(10, 1, 7, 8, '2018-05-04 02:45:00', '');
+INSERT INTO `track_eyes` VALUES(11, 1, 5, 8, '2018-05-04 02:50:00', '');
+INSERT INTO `track_eyes` VALUES(12, 1, 5, 8, '2018-05-04 02:55:00', '');
+INSERT INTO `track_eyes` VALUES(13, 1, 7, 8, '2018-05-04 03:00:00', '');
+INSERT INTO `track_eyes` VALUES(14, 2, 1, 1, '2018-05-04 02:35:00', '');
+INSERT INTO `track_eyes` VALUES(15, 2, 1, 1, '2018-05-04 02:40:00', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
