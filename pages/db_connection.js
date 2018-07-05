5dc6ef42ee59bba85eac052d8b5a52481ccedc20
Yige Dou <douyige@gmail.com>
2018-07-05 16:17:25 -0500

    updates
    db connection with pug

--- pages/db_connection.js
+++ pages/db_connection.js
@@ -1,22 +1,51 @@
+var express = require('express');
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-        host: "localhost",
-        //port: 22,
-        user: "SeniorDesign",
-        password: "SeniorDesign",
-        database: "seniordesign"
-});
+var app = express();
 
-con.connect(function(err) {
-        if(err) throw err;
-        else{
-        console.log("Connected!");
-        con.query("SELECT * FROM theaters", function(err, result) {
-                if(err) throw err;
-                console.log(result);
-                });
+function getMySQLConnection() {
+	return mysql.createConnection({
+                host: "localhost",
+                //port: 22,
+                user: "SeniorDesign",
+                password: "SeniorDesign",
+                database: "seniordesign"
+	});
+}
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req,res) {
+    var list = [];
+
+    var connection = getMySQLConnection();
+    connection.connect();
+
+    connection.query('SELECT * FROM theaters', function(err, rows, fields) {
+        if (err) {
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+        } else {
+            // Loop check on each row
+            for (var i = 0; i < rows.length; i++) {
+
+                // Create an object to save current row's data
+                var theater = {
+                    'id':rows[i].ID,
+                    'status':rows[i].STATUS,
+                    'movie':rows[i].MOVIE,
+                    'time':rows[i].REMAINING_TIME
+                }
+                // Add object into array
+                list.push(theater);
         }
+
+        res.render('index', {"list": list});
+        }
+    });
+
+connection.end();
 });
 
-//process.exit();
+app.listen(8080, function() {
+    console.log('listening on port', 8080);
+});
