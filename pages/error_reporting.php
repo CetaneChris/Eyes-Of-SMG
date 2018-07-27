75ca4ccf234ec9e1c9d02096448487c515d1be49
CetaneChris <christopher.raymond@mavs.uta.edu>
2018-07-27 10:11:26 -0500

    Demo test

--- pages/error_reporting.php
+++ pages/error_reporting.php
@@ -15,7 +15,41 @@
                     <i class="fas fa-exclamation-circle fa-fw"></i> Errors
                 </div>
                 <div class="panel-body">
-                    
+                    <table id="errors" class="table table-striped table-bordered"><?php
+						$query = "select log_id, error_type, description, time_found, description from error_log order by log_id asc";
+			        
+			        	$result = $mysqli->query($query);
+			            
+			        	//display column headers
+			            echo "<thead>";
+				        	echo "<th style='text-align:center' width=\"" . 100/(mysqli_num_fields($result)+3) . "%\">Error Type</th>";
+				            echo "<th style='text-align:center' width=\"" . 4*(100/(mysqli_num_fields($result)+3)) . "%\">Description</th></tr>";
+				        	echo "<th style='text-align:center' width=\"" . 100/(mysqli_num_fields($result)+3) . "%\">Time Found</th>";
+				        	echo "<th style='text-align:center' width=\"" . 100/(mysqli_num_fields($result)+3) . "%\">Status</th>";
+			            echo "</thead>";
+			
+			            //display the data
+			            echo "<tbody>";
+			            	while($row = mysqli_fetch_array($result)){
+			                  	echo "<tr>";
+			                  	
+			                  	//Error Type
+			                  	echo "<td align='center' style='padding: 15px'>" . $row['error_type'] . "</td>";
+			                  	
+			                  	//Description
+			                  	echo "<td align='center' style='padding: 15px'>" . $row['description'] . "</td>";
+			                  	
+			                  	//Time Found
+						    	echo "<td align='center' style='padding: 15px'>" . $row['time_found'] . "</td>";
+						    	
+						    	//Status
+		                  		echo "<td align='center' style='padding: 15px'>" . $row['status'] . "</td>";
+
+		                  		echo "</tr>";
+			                  }
+			                  ?>   
+			            </tbody>
+					</table>
                 </div>
             </div>
         </div>
