7237ac3d5a509b502f4b8642d2dddf44f3544510
CetaneChris <christopher.raymond@mavs.uta.edu>
2018-08-06 16:58:39 -0500

    error correction

--- pages/footer.php
+++ pages/footer.php
@@ -6,12 +6,10 @@
     <script src="/vendor/datatables/js/datatables.min.js"></script>
     <script src="/vendor/blackrock-digital/js/sb-admin-2.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/vendor/nightops/nightops.js"></script>
     <script src="/vendor/metisMenu/metisMenu.min.js"></script>
     <script src="/vendor/morrisjs/morris.min.js"></script>
     <script src="/vendor/raphael/raphael.min.js"></script>
-    <script src="/vendor/nightops/nightops.js"></script>
 	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-	<script type="text/javascript" src="/vendor/jquery/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="/vendor/moment/moment.js"></script>
 </body>
 </html>
