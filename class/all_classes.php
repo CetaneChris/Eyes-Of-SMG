92d35effd31e9017623754501573eefc5f066adc
Yige Dou <douyige@gmail.com>
2018-08-15 19:22:31 -0500

    deleted class directory

--- class/all_classes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once ($_SERVER['DOCUMENT_ROOT']."/class/devices.php");
-include_once ($_SERVER['DOCUMENT_ROOT']."/class/site_variables.php");
-include_once ($_SERVER['DOCUMENT_ROOT']."/class/theaters.php");
-
-?>
